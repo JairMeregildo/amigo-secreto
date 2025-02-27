848a16c30e42428223e89410b07b85424284f63c
Jair <jairmeregildo18@gmail.com>
2025-02-27 10:22:26 -0500

    actualiza el app.js

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
+let nombres = [];
 
+function agregarAmigo(){
+    let amigos = document.querySelector('#amigo').value;
+    document.querySelector('#resultado').innerHTML='';
+    if (amigos === '') {  // Validación para que no se agregue un valor vacío
+        alert('Por favor, ingrese un nombre valido')
+    }else{
+        nombres.push(amigos); // Si no esta vacio agrega la lista de amigos
+        mostrar(); // Actualiza la lista de amigos
+    }
+    document.querySelector('#amigo').value=''; // Limpiar el campo de entrada después de agregar
+}
+
+function mostrar(){
+    let lista = document.querySelector("#listaAmigos");
+    lista.innerHTML=''; // Limpiar la lista despues de agregar nuevos amigos
+    for(let elementoActual of nombres){
+        lista.innerHTML += '<li>' + elementoActual + '</li>';
+    }
+}
+
+function sortearAmigo(){
+    if(nombres.length === 0){
+        document.querySelector('#resultado').innerHTML='<p>No hay amigos para sortear </p>';
+    }else{
+        let sorteo = Math.floor(Math.random()*nombres.length);
+        document.querySelector('#resultado').innerHTML='<p>El amigo secreto sorteado es: '+ nombres[sorteo]+'</p>';
+    }
+    
+    nombres = [];
+    mostrar();
+}
